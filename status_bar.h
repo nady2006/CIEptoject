8060d22294a315b237e8a0be7a59788e5d83db63
AnasXHunter <anas.antar.fath@gmail.com>
2025-04-26 20:53:49 +0300

    Add files via upload

---
+++ status_bar.h
@@ -0,0 +1,16 @@
+#pragma once
+class status{
+    string * MenuItems;
+    string* s;
+    int* n;
+    color* colors;
+    int size;
+    int MenuItemWidth;
+    int Height;
+    window w;
+public:
+    status(int x = 4, int width = 50, int height = 50); 
+    void set_images(int i, string y); 
+    void set_status(int i, int x); 
+    void DrawMenuDemo(window& w);
+};
